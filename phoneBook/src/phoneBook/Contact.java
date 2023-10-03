2df81dd678430ce63b56dcd1e9fe99928d4fb0ba
abdallhal-mutiri <abdallhal-mutiri@localhost>
2023-10-03 05:20:22 +0300

    pushing all classes [class contact is done]

--- phoneBook/src/phoneBook/Contact.java
+++ phoneBook/src/phoneBook/Contact.java
@@ -1,7 +1,81 @@
 package phoneBook;
 
-public class Contact {
+public class Contact implements Comparable<Contact>  {
 
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String address;
+	private String notes;
+	
+	public Contact(String name, String phoneNumber, String email, String address, String notes) {
+
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.address = address;
+		this.notes = notes;
+	}
+
+
+	@Override
+	public int compareTo(Contact o) {
+		return this.name.compareTo(o.getName()) ;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getNotes() {
+		return notes;
+	}
+
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+	
+	
+	
 	
 
 }
