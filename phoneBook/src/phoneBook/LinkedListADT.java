084bbeebfa98b897e158bb19f88da878b5415534
Meshh143 <146827737+Meshh143@users.noreply.github.com>
2023-10-14 12:27:24 +0300

    Update LinkedListADT.java
    double linked list + size
    just edited insert and remove methods

--- phoneBook/src/phoneBook/LinkedListADT.java
+++ phoneBook/src/phoneBook/LinkedListADT.java
@@ -4,8 +4,12 @@ package phoneBook;
 public class LinkedListADT<T>  implements list<T>{
     private Node<T> head;
     private Node<T> current;
+    private Node<T> previous;
+    public int size;
     
-
+    public int getSize() {
+		return size;
+	}
     // Return ture when the list empty.
     public boolean isEmpty() {
         return head == null;
@@ -26,6 +30,10 @@ public class LinkedListADT<T>  implements list<T>{
        current = current.getNext();
     }
 
+    public void findpevious()throws NullPointerException {
+    	current = current.getPrevious();
+    }
+
     // return true when the current referencing at last node.
     public boolean last()throws NullPointerException {
       return current.getNext() == null;
@@ -42,14 +50,31 @@ public class LinkedListADT<T>  implements list<T>{
 
     // Resive data, and, insert it at the next node of the current node.
     public void insert(T data){
-       Node<T> newNode = new Node<T>(data);
-      
-       if(isEmpty())
-        current = head = newNode;
-
-       newNode.setNext(current.getNext());
-       current.setNext(newNode);
-       current = newNode;
+    	Node<T> newNode = new Node<T>(data);
+    	if(isEmpty()) {
+    		head = newNode;
+    	}
+    	else if(current==head){
+    		newNode.setNext(head);
+    		head.setPrevious(newNode);
+    		head=newNode;
+    	}
+    	else{
+    		Node<T>	previous = current.getPrevious();
+    		newNode.setNext(current);
+    		newNode.setPrevious(previous);
+    		previous.setNext(newNode);
+    		current.setPrevious(newNode);
+    	}
+    	current = newNode;
+    	size++;
+    	
+    }
+    public void insertAtEnd(T data) {
+    	Node<T> newNode = new Node<T>(data);
+    	current.setNext(newNode);
+		newNode.setPrevious(current);
+		size++;
     }
 
     // Deletes the element at the current.
@@ -58,25 +83,26 @@ public class LinkedListADT<T>  implements list<T>{
         if (isEmpty())
             return;
 
-        if(current == head)
+        if(current == head) {
             head = head.getNext();
+            head.setPrevious(null);
+            }
+        	
+        else if(last()) {
+        	Node<T> previous = current.getPrevious();
+        	previous.setNext(null);
+        	current.setPrevious(null);
+        	current=previous; 
+        }
       
         else{
-            Node<T> tempNode = head;
-
-            while(tempNode.getNext() != current){
-                tempNode.setNext(tempNode.getNext());
-            }
-
-            tempNode.setNext(current.getNext());
+            Node<T> previous = current.getPrevious();
+            previous.setNext(current.getNext());
+            current=current.getNext();
+            current.setPrevious(previous);
         }
   
-        if(last())
-          current = head;
-        
-        else
-          current = current.getNext();
-        
+        size--;
     }
 
 }
